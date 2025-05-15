ee85aee9f31542e4619c329514fc3e286c6dafd7
Apoorva-Sahyadri-ECE <nagekaranupama@gmail.com>
2025-05-16 00:46:12 +0530

    Create ledconstarints.v

---
+++ Task6/ledconstarints.v
@@ -0,0 +1,9 @@
+set_io clk     20  # Onboard 50MHz
+set_io uartrx  15  # UART RX from FTDI/Docklight
+set_io seg[0] 46  
+set_io seg[1] 2   
+set_io seg[2] 9   
+set_io seg[3] 10  
+set_io seg[4] 11  
+set_io seg[5] 19  
+set_io seg[6] 18
